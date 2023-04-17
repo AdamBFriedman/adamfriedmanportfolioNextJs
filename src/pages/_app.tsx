6843621a17f839de98f3fc463b7d512cbcdd3029
Adam Friedman <adam.friedman1@accesscfa.com>
2023-04-17 15:55:01 -0500

    Wrap app in Navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
+import Layout from "@/components/layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import "../styles/home.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
