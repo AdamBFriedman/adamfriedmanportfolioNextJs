8bc80b9f5a5d8211b637097d11f0e772b4ca94d0
Adam Friedman <adam.friedman1@accesscfa.com>
2023-04-18 11:22:28 -0500

    Update profile component

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,67 +1,30 @@
-import Image from "next/image";
-
-const ProfileCard = () => {
+const profileCard = () => {
   return (
-    <>
-      <div className={`aboutImageContainer rollInLeft`}>
-        <div className={"profileImageWrapper"}>
-          <Image
-            alt="Profile"
-            className={"profileImage"}
-            src={require("../images/profilePic.jpg")}
-          />
-          <ul className={"profileURL"}>
-            <li className={"profileLI"}>
-              <a
-                className={`profileA linkedIn`}
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://www.linkedin.com/in/adam-friedman5/"
-              >
-                LinkedIn
-                <span className="fa fa-linkedin"></span>
-              </a>
-            </li>
-            <li className={"profileLI"}>
-              <a
-                className={`profileA github`}
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://github.com/AdamBFriedman"
-              >
-                <span className="fa fa-github"></span>
-              </a>
-            </li>
-            <li className={"profileLI"}>
-              <a
-                className={`profileA gitlab`}
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://gitlab.com/Kustanza"
-              >
-                <span className="fa fa-gitlab"></span>
-              </a>
-            </li>
-            <li className={"profileLI"}>
-              <a
-                className={`profileA gmail`}
-                href="mailto:adam.friedman5@gmail.com?subject=Let's chat!&body=Hi Adam..."
-              >
-                <span className="fa fa-envelope-o"></span>
-              </a>
-            </li>
-          </ul>
-          <div className={"profileInfo"}>
-            <h2 className={"profileInfoHeader"}>Adam Friedman</h2>
-            <span className={"profileInfoSubHeader"}>
-              Senior Web Developer{" "}
-              <span className={"profileInfoLocation"}>San Francisco, CA</span>
-            </span>
+    <div className="profileContainer">
+      <div className="profileWrapper">
+        <div className="profilePanel">
+          <div className="profileRing">
+            <div className="profileCard"></div>
+            <div className="profileBorder">
+              <div className="profileSlide">
+                <h6 className="profilePara">Info</h6>
+                <div className="profileLine">
+                  <h6 className="profilePara">Portfolio</h6>{" "}
+                  <i className="fa fa-plane" aria-hidden="true"></i>
+                  <h6 className="profilePara">LinkedIn</h6>
+                </div>
+                <div className="profileLine">
+                  <h6 className="profilePara">GitHub</h6>{" "}
+                  <i className="fa fa-plane" aria-hidden="true"></i>
+                  <h6 className="profilePara">GitLab</h6>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
-export default ProfileCard;
+export default profileCard;
