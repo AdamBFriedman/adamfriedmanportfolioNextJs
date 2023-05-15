603ef5c0cd6a9cfe82a6eb4f5d48ded803dde5eb
Adam Friedman <adam.friedman1@accesscfa.com>
2023-05-15 14:03:50 -0500

    Style profile card and skills for mobile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,13 +1,13 @@
-import type { RootState } from "@/store/store";
 import { useSelector } from "react-redux";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import EmailIcon from "@mui/icons-material/Email";
 import Link from "next/link";
+import { isDarkModeStore } from "./Navbar";
 
 const Profile = () => {
   const isDarkMode = useSelector(
-    (state: RootState) => state.isDarkMode.isDarkMode
+    (state: isDarkModeStore) => state.isDarkMode.isDarkMode
   );
 
   return (
